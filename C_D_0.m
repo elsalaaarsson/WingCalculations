ea17d49728a8b413cda75e5bc529a4cd851bce7d
Elsa Larsson <elsa.laaarsson@gmail.com>
2020-06-26 15:37:40 +0100

    Initial commit of MATLAB files

---
+++ C_D_0.m
@@ -0,0 +1,4 @@
+function out = C_D_0()
+drag_coeff = C_f * FF * S_wet / S_ref;
+out = drag_coeff;
+end
---
+++ WingDesign.m
@@ -0,0 +1,77 @@
+% General constants
+g = 9.81;       % Gravitational acceleration [m/s2]
+rho_air = 1.28; % Density of air at 15˚C [kg/m3]
+
+% Design generic
+m = 6;          % Mass of the drone [kg]
+v_cruise = 30;  % Cruise speed [m/s]
+v_stall = 5;   % Stall speed  - arbitrarily chosen! [m/s]
+% - calculated values -
+W = m * g;  % Weight of the drone [N]
+
+% Body and wings
+S = 0.55;       % Wing area [m2]
+AR = 9;         % Aspect ratio
+lambda = 0.72;  % Taper ratio
+% - calculated values - 
+b = sqrt(S * AR);  % Wing span [m]
+c = S / b;  % Mean chord length [m]
+c_r = (2 / (1 + lambda)) * c;  % Root chord [m]
+c_t	= c_r * lambda;  % Tip chord [m]
+h = 0.15 * c;  % Thickness [m]
+length_fuselage = 0.4;  % Fuselage length [m]
+dia_fuselage = 0.1;  % Fuselage diameter [m]
+ang_quarter_chord = atan((c_r - c_t) / 4 / ((b - dia_fuselage) / 2));  % Quarter chord sweep angle [rad]
+C_l_max = 1.2731;  % Airfoil maximum lift coefficient
+
+% Propellers
+N = 4;          % Number of propellers
+D = 1;          % Diameter of propeller span [m] -- EDIT!!
+C_07R = 1;      % Coefficient of drag of 0.7 length of the propeller -- EDIT!!
+
+
+% Lift coefficients
+C_L_max = 0.9 * C_l_max * cos(ang_quarter_chord);  % Max lift coef. for the whole VTOL
+
+% List of components
+% - Wing, inner (untapered)
+% - Wing, outer (tapered)
+% - Fuselage
+% - Tail and tailplane
+% - Tail motor
+% - Winglets
+% - VTOL propellers
+% - VTOL motors
+% - Booms
+
+% Skin friction coefficients
+% 
+
+
+% Form factors
+
+
+% Drag properties for all components
+f = length_fuselage / dia_fuselage;  % Ratio of characteristic lengths
+
+% Zero-lift drag coefficients per component
+C_D_0_fuselage = drag_coefficient_0(C_f_fuselage, FF_fuselage, S_wet_fuselage, S_ref);
+
+% Wing loading, 2 methods:
+% Stall speed constraint
+wing_loading_1 = 0.5 * rho_air * v_stall^2 * C_L_max;
+%
+% Cruise speed constraint
+e = 0.7;  % Oswald efficiency approximation for "extended slats, flaps and landing gear". Deemed equivalent of having propellers deployed.
+K = 1 / (pi() * AR * e);  % Aerodynamic factor
+C_D_induced = K * (2 * W / (v_cruise^2 * rho_air * S))^2;  % Induced drag coefficient
+C_D_0_prop = 0.1 * N * D * C_07R / S;  % Drag coefficient from VTOL propellers
+
+% Zero-lift drag coefficient
+C_D_0 = C_D_0_fuselage + C_D_0_prop;  % Zero-lift parasite drag coefficient, ignoring interference factor
+
+% Drag difference from zero-lift (deployment of flaps etc)
+% Depends on the point of flight
+
+% Total drag
+C_D = C_D_0 + C_L^2 / (pi() * AR * e);
